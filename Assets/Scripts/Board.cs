c218197fddfc13eb3e62356fb03bf3b304724c86
Jon Whisman <jon@pixelgraft.com>
2022-06-01 21:17:31 -0700

    Demo assets imported. Initial Scene setup with prefab and script for board

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+
+{
+    public int width;
+    public int height;
+
+    public GameObject bgTilePreFab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
