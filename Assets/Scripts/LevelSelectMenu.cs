d75f17d1949b60604e835892cb26e54698086cc8
Jon Whisman <jon@pixelgraft.com>
2022-06-22 21:21:10 -0700

    Upgraded to Unity 2021.2.7f1

--- Assets/Scripts/LevelSelectMenu.cs
+++ Assets/Scripts/LevelSelectMenu.cs
@@ -8,6 +8,13 @@ public class LevelSelectMenu : MonoBehaviour
 
     public string mainMenu;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C)){
+            PlayerPrefs.DeleteAll();
+        }
+    }
+
     public void MainMenu()
     {
         SceneManager.LoadScene(mainMenu);
