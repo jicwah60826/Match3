62bd48ea8a293c90872f15639c389a923761d674
Jon Whisman <jon@pixelgraft.com>
2022-07-14 20:52:33 -0700

    fixed all buttons and changes button graphics to more pastel

--- Assets/Scripts/LevelSelectButton.cs
+++ Assets/Scripts/LevelSelectButton.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelectButton : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class LevelSelectButton : MonoBehaviour
     public string levelToLoad;
 
     public GameObject star1, star2, star3;
+
+    public Button levelButton;
 
     // Start is called before the first frame update
     void Start()
